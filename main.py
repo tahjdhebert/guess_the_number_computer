106a8ed3bfe465cefcffe4afdbc96758ae40681f
tahjdhebert <the3lastlin3@gmail.com>
2023-09-13 15:24:49 -0500

    A file appears

---
+++ main.py
@@ -0,0 +1,8 @@
+'''
+Author: T D Hebert
+Date: 2023-09-13
+The purpose of this program is for the user to think of a number between 1 and 10, and for the machine to guess until it is correct
+'''
+
+
+    
